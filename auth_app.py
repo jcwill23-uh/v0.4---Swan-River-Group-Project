f6be20fae7061b64cb28a183167eede4f5b2cc0f
Haseebs1 <hshahza3@cougarnet.uh.edu>
2025-02-09 12:56:40 -0600

    Update auth_app.py

--- auth_app.py
+++ auth_app.py
@@ -1,43 +1,68 @@
-from flask import Flask, redirect, url_for, session
-from authlib.integrations.flask_client import OAuth
+from flask import Flask, redirect, url_for, session, request, render_template
+from flask_oauthlib.client import OAuth
+from functools import wraps
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'  
-oauth = OAuth(app)
+app.secret_key = 'swanRiver'  # Replace with a strong, random secret key
 
-azure = oauth.register(
-    name='SwanRiver',
-    client_id='e6265ffc-86da-4716-985b-a0df698c90b7',
-    client_secret='2OG8Q~~FF3kcyJCd3fp4MQa67FIbcZEmelrV4bHH',  
-    authorize_url='https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-    token_url='https://login.microsoftonline.com/common/oauth2/v2.0/token',
-    userinfo_endpoint='https://graph.microsoft.com/oidc/userinfo',
-    client_kwargs={
-        'scope': 'openid profile email'
-    }
+oauth = OAuth(app)
+azure = oauth.remote_app(
+    'azure',
+    consumer_key='7d3a3c1c-46ec-4247-9ed4-ef0d1526c5b9',  # Replace with your Application (client) ID
+    consumer_secret='1pF8Q~cPp9z-i_1N3gkeN4FN4t3gT9_7fcl-Tcek',  # Replace with your Client Secret
+    request_token_params={
+        'scope': 'User.Read'
+    },
+    base_url='https://graph.microsoft.com/v1.0/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://login.microsoftonline.com/170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259/oauth2/token',  # Replace {tenant_id} with your Directory (tenant) ID
+    authorize_url='https://login.microsoftonline.com/170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259/oauth2/v2.0/authorize'  # Replace {tenant_id} with your Directory (tenant) ID
 )
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'azure_token' not in session:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def index():
-    return 'Welcome to the Flask app'
+    if 'azure_token' in session:
+        return redirect(url_for('admin'))
+    return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
-    redirect_uri = url_for('authorize', _external=True)
-    return azure.authorize_redirect(redirect_uri)
+    return render_template('login.html')
 
-@app.route('/authorize')
-def authorize():
-    token = azure.authorize_access_token()
-    user_info = azure.parse_id_token(token)
-    session['user'] = user_info
-    return f'Hello, {user_info["name"]}!'
+@app.route('/login/authorize')
+def login_authorize():
+    return azure.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/logout')
 def logout():
-    session.pop('user', None)
+    session.pop('azure_token', None)
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/authorized')
+@azure.authorized_handler
+def authorized(resp):
+    if resp is None or 'access_token' not in resp:
+        return redirect(url_for('login'))
+    session['azure_token'] = resp['access_token']
+    return redirect('https://jcwill23-uh.github.io/Swan-River-Group-Project/admin.html')
 
+@app.route('/admin')
+@login_required
+def admin():
+    return render_template('admin.html')
+
+@azure.tokengetter
+def get_azure_token():
+    return session.get('azure_token')
+
+if __name__ == '__main__':
+    app.run(debug=True)
