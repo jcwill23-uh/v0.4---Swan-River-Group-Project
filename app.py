275f15d936c4dee64fd9077311350c36ea360793
John Williams <jcwill23@cougarnet.uh.edu>
2025-02-16 12:38:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -210,4 +210,4 @@ def deactivate_user(user_id):
 # **Place this block at the bottom**
 if __name__ == '__main__':
     setup_db()  # Initialize database tables
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
