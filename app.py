b6a36c3e5fa2fec72235f6d184c87f145a005eaf
Haseebs1 <hshahza3@cougarnet.uh.edu>
2025-02-17 00:53:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ app = Flask(__name__, template_folder='docs', static_folder='docs')
 app.secret_key = os.getenv('SECRET_KEY')  # Required for session management
 
 # Azure AD configuration
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-TENANT_ID = os.getenv('TENANT_ID')
+CLIENT_ID = "7fbeba40-e221-4797-8f8a-dc364de519c7"
+CLIENT_SECRET = "x2T8Q~yVzAOoC~r6FYtzK6sqCJQR_~RCVH5-dcw8"
+TENANT_ID = "170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259"
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-REDIRECT_URI = os.getenv('REDIRECT_URI')
+REDIRECT_URI = 'https://swan-river-group-project.azurewebsites.net/auth/callback'
 SCOPE = ['User.Read']
 
 # Secure configuration settings
