d01a5756996343d9de24c6998715d1f8845882a1
Kancherla, Rohan <rohan360k@gmail.com>
2025-02-18 22:40:06 -0600

    Update Queries.py

--- Queries.py
+++ Queries.py
@@ -4,7 +4,7 @@ import pyodbc as db
 def AddUser(string):
   data = [v for k, v in json.loads(string).items()]
   e = None
-  with db.connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:swan-river.database.windows.net,1433;Database=SRDB;Uid=TRD159;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;") as conn:
+  with db.connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:swan-river-user-information.database.windows.net,1433;Database=UserDatabase;Uid=jcwill23@cougarnet.uh.edu@swan-river-user-information;Pwd=H!ghLander;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;") as conn:
     with conn.cursor() as cursor:
       try:
         cursor.execute("INSERT INTO Users VALUES (?, ?, ?, ?)", (data[0], data[1], data[2], data[3]))
