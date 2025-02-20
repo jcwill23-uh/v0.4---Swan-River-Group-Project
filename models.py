d30eec05e32adcc5e1393688933bb8dc4c013b0e
John Williams <jcwill23@cougarnet.uh.edu>
2025-02-19 22:18:46 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,11 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 
-'''class User(db.Model):
+db = SQLAlchemy()
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     role = db.Column(db.String(50), default="basicuser")
-    status = db.Column(db.String(20), default="active")  # "active" or "deactivated"
-
-    def __repr__(self):
-        return f"<User {self.name}>"
-
-
-def is_admin(user_email):
-    user = User.query.filter_by(email=user_email).first()
-    return user and user.role == "admin"'''
-  
+    status = db.Column(db.String(20), default="active")
+    
