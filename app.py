4673ff69a83d8b4f85f89ea0cd307278f7cdf294
Haseebs1 <hshahza3@cougarnet.uh.edu>
2025-02-20 14:31:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ def _get_user_info(token):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8000, debug=
+    app.run(host='0.0.0.0', port=8000, debug=True)
