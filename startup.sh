d1e982b5534664988451cbcd21a53f5fea63243d
John Williams <jcwill23@cougarnet.uh.edu>
2025-03-14 14:24:05 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,14 @@
-# Update package list and install LaTeXapt
-get update && apt-get install -y texlive-latex-base texlive-fonts-recommended texlive-extra-utils texlive-latex-extra
+#!/bin/bash
 
-# Start Gunicorn to serve Flask app
+# Install dependencies explicitly from requirements
+pip install -r requirements.txt
+
+# Install necessary LaTeX packages (clearly already defined correctly by you)
+apt-get update && apt-get install -y \
+    texlive \
+    texlive-latex-extra \
+    texlive-fonts-recommended \
+    texlive-extra-utils
+
+# Run the Flask application explicitly with Gunicorn
 gunicorn -w 4 -b 0.0.0.0:8000 app:app
