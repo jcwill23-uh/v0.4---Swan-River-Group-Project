a9b214261363ccc518fdc81d51b144e28d432eae
John Williams <jcwill23@cougarnet.uh.edu>
2025-03-14 10:45:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Azure AD Configuration
-CLIENT_ID = "7fbeba40-e221-4797-8f8a-dc364de519c7"
-CLIENT_SECRET = "x2T8Q~yVzAOoC~r6FYtzK6sqCJQR_~RCVH5-dcw8"
-TENANT_ID = "170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259"
-AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-REDIRECT_URI = "https://swan-river-group-project.azurewebsites.net/auth/callback"
-SCOPE = ['User.Read']
+class Config:
+  CLIENT_ID = "7fbeba40-e221-4797-8f8a-dc364de519c7"
+  CLIENT_SECRET = "x2T8Q~yVzAOoC~r6FYtzK6sqCJQR_~RCVH5-dcw8"
+  TENANT_ID = "170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259"
+  AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
+  REDIRECT_URI = "https://swan-river-group-project.azurewebsites.net/auth/callback"
+  SCOPE = ['User.Read']
+  
