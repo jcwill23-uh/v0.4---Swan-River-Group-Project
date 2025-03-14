f9212ea295a4f19aa2f9776806cefe783b1f8728
John Williams <jcwill23@cougarnet.uh.edu>
2025-03-14 11:59:16 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+# Update package list and install LaTeXapt
+get update && apt-get install -y texlive-latex-base texlive-fonts-recommended texlive-extra-utils texlive-latex-extra
 
-# Start Gunicorn Server
+# Start Gunicorn to serve Flask app
 gunicorn -w 4 -b 0.0.0.0:8000 app:app
