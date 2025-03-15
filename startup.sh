78425436614e62b949befca5e670ac33022a7497
John Williams <jcwill23@cougarnet.uh.edu>
2025-03-14 22:12:02 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-pip install -r requirements.txt
 
-# Update package list and install LaTeX
+# Update package list and install system dependencies
 apt-get update && apt-get install -y \
     texlive-latex-base \
     texlive-latex-recommended \
     texlive-fonts-recommended \
-    texlive-extra-utils
+    texlive-extra-utils \
+    texlive-latex-extra \
+    latexmk
 
-# Start Gunicorn to serve Flask app
+# Install Python dependencies AFTER system dependencies
+pip install -r requirements.txt
+
+# Start Gunicorn with increased timeout for large requests
 gunicorn -w 4 -b 0.0.0.0:8000 app:app
