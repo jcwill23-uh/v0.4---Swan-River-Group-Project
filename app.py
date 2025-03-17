236ad9ba8873628e4834f7e2e821c64f63e9c1ec
John Williams <jcwill23@cougarnet.uh.edu>
2025-03-16 23:08:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -645,7 +645,6 @@ def basic_user_form_status():
     # Fetch user's email from session
     email = session['user']['email']
     user = User.query.filter_by(email=email).first()
-    forms = ReleaseFormRequest.query.filter_by(user_id=user.id).all()
 
     # Fetch all forms (Drafts + Submitted Forms)
     user_full_name = f"{user.first_name} {user.middle_name or ''} {user.last_name}".strip()
@@ -653,7 +652,7 @@ def basic_user_form_status():
         ReleaseFormRequest.student_name.like(f"%{user_full_name}%")
     ).all()
     
-    return render_template("basic_user_form_status.html", user=session['user'], requests=requests, forms=forms)
+    return render_template("basic_user_form_status.html", user=session['user'], requests=requests)
 
 def download_signature(signature_url, user_id):
     """
