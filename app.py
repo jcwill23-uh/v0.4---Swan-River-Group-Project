c4294d868e5bf739a10e7ce484c9539fb12d7e6d
John Williams <jcwill23@cougarnet.uh.edu>
2025-03-25 17:25:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -472,8 +472,8 @@ CLIENT_ID = "7fbeba40-e221-4797-8f8a-dc364de519c7"
 CLIENT_SECRET = "x2T8Q~yVzAOoC~r6FYtzK6sqCJQR_~RCVH5-dcw8"
 TENANT_ID = "170bbabd-a2f0-4c90-ad4b-0e8f0f0c4259"
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-REDIRECT_URI = "https://swan-river-group-project.azurewebsites.net/auth/callback"
-#REDIRECT_URI = "http://localhost:5000/auth/callback"
+#REDIRECT_URI = "https://swan-river-group-project.azurewebsites.net/auth/callback"
+REDIRECT_URI = "http://localhost:8000/auth/callback"
 SCOPE = ['User.Read']
 
 # Authentication Routes
